5cf4cc7c5848edbccdf294e033bf567fc9e42f58
Sander <sandervannoort@persistence.nl>
2020-10-30 19:48:42 +0100

    No indentation in ini files

--- bin/set_path.sh
+++ bin/set_path.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"; cd ..; cd .. >/dev/null 2>&1 && pwd )"
 
-export PYTHONPATH=$DIR/iap:$DIR/webiap:$DIR/mconvert:$DIR/Pyfig
+export PYTHONPATH=$DIR/iap:$DIR/webiap:$DIR/mconvert:$DIR/pyfig
